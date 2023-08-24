3cba23b3013d4bb70b2bda0bd4405401e459f255
Arnold Sunneräng <arnold.sunnerang@iths.se>
2023-08-24 15:54:55 +0200

    terraform validated

--- main.tf
+++ main.tf
@@ -3,18 +3,54 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "3.70.0"
     }
   }
-
-  required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
-  features {}
+  # Configuration options
 }
 
-resource "azurerm_resource_group" "terraformer" {
-  name     = "terraformer"
-  location = "northeurope"
+resource "azurerm_resource_group" "terraformerresourcegroup" {
+  name     = "terraformerresourcegroup"
+  location = "North Europe"
+}
+
+resource "azurerm_log_analytics_workspace" "trganalyticsworkspace" {
+  name                = "trganalyticsworkspace"
+  location            = azurerm_resource_group.terraformerresourcegroup.location
+  resource_group_name = azurerm_resource_group.terraformerresourcegroup.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_container_app_environment" "trgcontainerappenvironment" {
+  name                       = "trgcontainerappenvironment"
+  location                   = azurerm_resource_group.terraformerresourcegroup.location
+  resource_group_name        = azurerm_resource_group.terraformerresourcegroup.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.trganalyticsworkspace.id
+}
+resource "azurerm_container_app" "terraformercontainerapp" {
+  name                         = "terraformercontainerapp"
+  container_app_environment_id = azurerm_container_app_environment.trgcontainerappenvironment.id
+  resource_group_name          = azurerm_resource_group.terraformerresourcegroup.name
+  revision_mode                = "Single"
+
+  template {
+    container {
+      name   = "terraformer"
+      image  = "mcr.microsoft.com/azuredocs/containerapps-helloworld:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+    }
+  }
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "terraformerregistry"
+  resource_group_name = azurerm_resource_group.terraformerresourcegroup.name
+  location            = azurerm_resource_group.terraformerresourcegroup.location
+  sku                 = "Basic"
+  admin_enabled       = true
 }
